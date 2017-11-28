ff7d55a515768abc06c142b5a89f9d9b8c2c2441
Игорь Звягинцев <i.zvyagintsev@rambler-co.ru>
2017-11-29 00:28:14 +0300

    css переменные

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
-import './index.css'
+import css from './index.css'
 
 export default class AddButton extends Component {
     render () {
-        return <button className='button add'>
-            <span className='symbol'>➕</span> Добавить правило замены
+        return <button className={css.button}>
+            <span className={css.symbol}>➕</span> Добавить правило замены
         </button>
     }
 }
